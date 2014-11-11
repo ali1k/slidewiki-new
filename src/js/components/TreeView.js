281e375c891b11a52b3056324f06e79ac6116b2a
ali1k <hyperir@gmail.com>
2014-11-11 13:52:06 +0100

    tree refactoring

--- src/js/components/TreeView.js
+++ src/js/components/TreeView.js
@@ -6,21 +6,22 @@ var TreeView= React.createClass({
   render: function(){
     //it has a fixed value
     var selector=this.props.selector;
+    //handling css classes
+    var isSelected= (this.props.nodes.type==this.props.selector.type && this.props.nodes.id==this.props.selector.id);
+    var nodeClasses="sw-tree-view-node";
+    if(isSelected){
+      nodeClasses += " sw-tree-view-selected";
+    }
+    //handling child nodes
     var childNodes;
-    var selectedClass='';
     if (this.props.nodes.children != null) {
       childNodes = this.props.nodes.children.map(function(node, index) {
-        if(node.type==selector.type && node.id==selector.id){
-          selectedClass="sw-tree-view-selected ";
-        }else{
-          selectedClass="";
-        }
-        return <li className={selectedClass} key={index}><TreeView nodes={node} selector={selector}/></li>
+        return <li key={index}><TreeView nodes={node} selector={selector}/></li>
       });
     }
     return (
       <div className="sw-tree-view">
-        <h4 onClick={this._onClick}> {this.props.nodes.title} </h4>
+        <h4 onClick={this._onClick} className={nodeClasses}> {this.props.nodes.title} </h4>
         <ul>
           {childNodes}
         </ul>
