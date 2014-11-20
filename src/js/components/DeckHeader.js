ae51e57a0a00c4e6bbdaea39e6271c89b6725718
Ali Khalili <hyperir@gmail.com>
2014-11-20 16:10:32 +0100

    completed initial set of components

---
+++ src/js/components/DeckHeader.js
@@ -0,0 +1,41 @@
+var React=require('react');
+//stores
+
+//SlideWiki components
+
+
+function getDeckHeaderState() {
+  return {
+
+  };
+}
+
+var DeckHeader= React.createClass({
+  //list of properties for validation
+  propTypes: {
+
+  },
+  getInitialState: function() {
+    return getDeckHeaderState();
+  },
+  componentWillMount: function() {
+
+  },
+  componentDidMount: function() {
+
+  },
+  componentWillUnmount: function() {
+
+  },
+  render: function(){
+    return (
+      <div className="sw-deck-header">
+        <h2> DeckHeader </h2>
+      </div>
+    )
+  },
+  _onChange: function() {
+
+  }
+})
+module.exports= DeckHeader;
