76a3de8134f5ebfd423e7d3244c882db370a3b67
DaryaTarasowa <darya.tarasowa@gmail.com>
2015-02-17 21:54:07 +0100

    tree actions finally commited :(

---
+++ src/actions/TreeActions.js
@@ -0,0 +1,35 @@
+'use strict'
+var async = require('async');
+var TreeStore = require('../stores/TreeStore');
+
+exports.deleteFrom = function(context, payload, done){
+    context.dispatch('DELETE_FROM', payload);
+    done();
+};
+
+exports.addEmptySlide = function(context, payload, done){
+    context.dispatch('ADD_EMPTY_SLIDE', payload);
+    done();
+};
+
+exports._onDragStart = function(context, payload, done){
+    context.dispatch('ON_DRAG_START', payload);
+    done();
+};
+
+exports.checkDropPossible = function(context, payload, done){
+    context.dispatch('CHECK_DROP_POSSIBLE', payload);
+    context.dispatch('MOVE_ITEM', payload);
+    done();
+};
+
+exports._onDrop = function(context, payload, done){
+    context.dispatch('ON_DROP', payload);
+    done();
+};
+        
+
+
+
+
+
